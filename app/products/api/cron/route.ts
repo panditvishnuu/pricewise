59ad3eee25e9ac8e3cc047ded25a1dea2e76fc03
panditvishnuu <vishnusharma76434@gmail.com>
2024-08-13 21:10:41 +0530

    changed maxduration

--- app/products/api/cron/route.ts
+++ app/products/api/cron/route.ts
@@ -11,7 +11,7 @@ import Product from "@/lib/models/product.model";
 import { scrapedAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 60; // This function can run for a maximum of 300 seconds
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
